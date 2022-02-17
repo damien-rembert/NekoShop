2512c2da0bfd925131b5b03e3cae74ba94b4f4ef
Damien R <damien.rembert@gmail.com>
2022-02-17 11:37:16 +0000

    replacing the id value with faker names

--- src/components/CatList.js
+++ src/components/CatList.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Cat from './Cat';
+import Faker from 'faker';
 
 const CatList = ({cats}) => {
+
+    // let randomName = faker.name.findName();
+    cats.map( (cat) => {
+        cat.id = Faker.name.findName().toString();
+        // console.log(cat.name);
+
+    });
+    // console.log(cats);
+
+    
+
 return (
     <div>
         {cats.map((cat, index) => {
