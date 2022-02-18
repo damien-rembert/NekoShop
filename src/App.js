d61bf80227d3e54d3751f08472251b5efac12d8b
Damien R <damien.rembert@gmail.com>
2022-02-18 13:43:14 +0000

    build

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
 
     const collectCats = async () => {
     try {
-      const response = await fetch ("https://api.thecatapi.com/v1/images/search?limit=10")
+      const response = await fetch ("https://api.thecatapi.com/v1/images/search?limit=12")
       // console.log(response)
 
     if (response.status !== 200) {
