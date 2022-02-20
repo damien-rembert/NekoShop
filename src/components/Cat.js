61d4e865d2658f7b2ab0baf1b6d0f8d82064dc84
Damien Rembert <damien.rembert@gmail.com>
2022-02-20 15:43:05 +0000

    added visual cue when adding a cat

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,39 +1,32 @@
 // @ts-check
 
-import React from "react";
+import React, { useState } from "react";
 
 
 const Cat = ({name, price, url, addToBasket, index}) => {
+ 
 
     // {basket.length === 0 ? <div>No kitties in your basket</div> : <div>Total: £{sumOfBasket}</div>}
+    let [addedToBasket, setAddedToBasket] = useState(false);
     const addCat = (index) => {
         addToBasket(index);
-
+        setAddedToBasket(true);
+        setTimeout(() => {setAddedToBasket(false)}, 400);
     }
-
-
-
 
     return (
         <div className="cat-card">
             <p>{name}</p>
             <p>£{price}</p>
             <img src={url} alt='just another cat' ></img>
-            <button onClick={() => addCat(index)}>Add to basket</button>
+
+            {addedToBasket ? <button >Added to basket</button> : <button onClick={() => addCat(index)}>Add to basket</button>}
+
+
+            
+            
         </div>
     )
 }
-
-// const Cat = ({cat, addToBasket, index}) => {
-
-//     return (
-//         <div className="cat-card">
-//             <p>{cat.id}</p>
-//             <img src={cat.url}></img>
-//             <button onClick={() => addToBasket(index)}>Add to basket</button>
-//         </div>
-//     )
-// }
-
 
 export default Cat;
